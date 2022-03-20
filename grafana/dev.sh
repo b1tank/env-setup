522e34367ddb490ee32c6d5186a852662d80fbd1
b1tank <b1tank@outlook.com>
2022-03-20 04:59:00 +0000

    add grafana dev resources

--- grafana/dev.sh
+++ grafana/dev.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# set up dev resources (data sources and dashboards)
+# ref: https://github.com/grafana/grafana/blob/main/contribute/developer-guide.md#add-data-sources
+cd devenv
+./setup.sh
+
 # backend: build and start
 make run
 
